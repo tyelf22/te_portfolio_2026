1e4bcd08aabfcee4a170f8c9c7e4b7f2c7725d04
Tyson <tyelf22@hotmail.com>
2026-02-27 13:24:02 -0700

    Update Sidebar component to use dynamic viewport height for improved layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 
 export default function Sidebar({ onNewChat, onClose }) {
     return (
-        <aside className="w-[260px] bg-chatgpt-sidebar flex flex-col h-screen">
+        <aside className="w-[260px] bg-chatgpt-sidebar flex flex-col h-dvh md:h-screen">
             <div className="px-2 pt-3 pb-2">
                 <button
                     onClick={() => {
